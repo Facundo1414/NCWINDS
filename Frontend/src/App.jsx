247bae04df48f772d5d15655c368b4edb9b82083
marcorios26 <riosmar026@gmail.com>
2024-04-02 21:51:49 -0400

    added atomic design methodology

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css'
 import { Routes,Route } from 'react-router-dom'
-import { Main } from './pages/Main'
-import Navbar from './utils/Navbar'
-import Footer from './utils/Footer'
+import { Main } from './pages/main/Main'
+import Navbar from './components/organisms/navbar/Navbar'
+import Footer from './components/organisms/footer/Footer'
 
 function App() {
   return (
       <>
         <Navbar/>
         <Routes>
-          <Route path="/" element={<Main></Main>}></Route> // main 
-          <Route></Route> // 
+          <Route path="/" Component={Main}></Route>
+          <Route></Route>  
         </Routes>
         <Footer/>
       </>
--- Frontend/src/pages/main/Main.jsx
+++ Frontend/src/pages/main/Main.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export const Main = () => {
     
   return (
-    <div>Main</div>
+    <div>Main ...</div>
   )
 }
