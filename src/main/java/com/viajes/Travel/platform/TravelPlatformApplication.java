3c8ec396c73fa1756bd9affe55d7c34aca005a1a
Luciana <lucianaagustinacoronel@gmail.com>
2024-04-03 20:14:46 -0300

    services modification

--- src/main/java/com/viajes/Travel/platform/TravelPlatformApplication.java
+++ src/main/java/com/viajes/Travel/platform/TravelPlatformApplication.java
@@ -1,8 +1,4 @@
 package com.viajes.Travel.platform;
-
-import com.viajes.Travel.platform.entity.viajes;
-import com.viajes.Travel.platform.respository.ViajesRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,16 +9,6 @@ public class TravelPlatformApplication {
 		SpringApplication.run(TravelPlatformApplication.class, args);
 	}
 
-	@Autowired
 
-	private ViajesRepository viajesRepository;
-
-	public Iterable<viajes> listAllTrips() {
-		return viajesRepository.findAll();
-	}
-
-	public Iterable<viajes> findByPrice(Double price){
-		return viajesRepository.findByPrice(price);
-	}
 
 }
--- src/main/java/com/viajes/Travel/platform/services/ViajesServices.java
+++ src/main/java/com/viajes/Travel/platform/services/ViajesServices.java
@@ -1,2 +1,20 @@
-package com.viajes.Travel.platform.services;public class ViajesServices {
+package com.viajes.Travel.platform.services;
+
+import com.viajes.Travel.platform.entity.viajes;
+import com.viajes.Travel.platform.respository.ViajesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ViajesServices {
+
+    @Autowired
+
+    private ViajesRepository viajesRepository;
+
+    public Iterable<viajes> listAllTrips() {
+        return viajesRepository.findAll();
+    }
+
+    public Iterable<viajes> findByPrice(Double price){
+        return viajesRepository.findByPrice(price);
+    }
 }
