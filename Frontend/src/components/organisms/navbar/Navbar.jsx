1f600ca3c1573180eb2d69b7bc2a0fa83c7abab2
Facundo1414 <facu.allende14gmail.com>
2024-04-03 16:00:07 -0300

    se le agrego estilos al navbar

--- Frontend/src/components/organisms/navbar/Navbar.jsx
+++ Frontend/src/components/organisms/navbar/Navbar.jsx
@@ -4,21 +4,27 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import './Navbar.css'
+
+//TODO: hacer que el navbar sea responsive
 
 const Navbar = () => {
   return (
-    <AppBar position="fixed" style={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
-      <Toolbar>
-        <Typography  variant="h6" component="div" sx={{ flexGrow: 1, color: 'white' }}>
-          FlyAirlines
+    <AppBar position="fixed" className="transparent-navbar">
+      <Toolbar className="toolBar">
+        <Typography  variant="h6" component="div" sx={{ flexGrow: 1, color: 'white' }} className="logoContainer">
+          <Link to="/" className="brand">FlyAirlines</Link>
         </Typography>
-        <Button component={Link} to="/" color="inherit">Destinos</Button>
-        <Button component={Link} to="/about" color="inherit">Paquetes</Button>
-        <Button component={Link} to="/contact" color="inherit">Información</Button>
-        <Button component={Link} to="/login" variant="outlined" color="inherit">Login</Button>
+        <div className="buttonContainer">          
+          <Button component={Link} to="/destinos" color="inherit" className="nav-link" >Destinos</Button>
+          <Button component={Link} to="/paquetes" color="inherit" className="nav-link">Paquetes</Button>
+          <Button component={Link} to="/informacion" color="inherit" className="nav-link">Información</Button>
+          <Button component={Link} to="/login" variant="outlined" color="inherit" className="login-btn" >Login</Button>
+        </div>
       </Toolbar>
     </AppBar>
   );
 };
+
 
 export default Navbar;
