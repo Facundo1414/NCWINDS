894adc6f8554bbb06aadf6279b5de0fa83266927
Facundo1414 <facu.allende14gmail.com>
2024-04-03 14:26:04 -0300

    estructura del navbar creada

--- Frontend/src/components/organisms/navbar/Navbar.jsx
+++ Frontend/src/components/organisms/navbar/Navbar.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 const Navbar = () => {
   return (
-    <div>
-        <p>navbar</p>
-    </div>
-  )
-}
+    <AppBar position="fixed" style={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
+      <Toolbar>
+        <Typography  variant="h6" component="div" sx={{ flexGrow: 1, color: 'white' }}>
+          FlyAirlines
+        </Typography>
+        <Button component={Link} to="/" color="inherit">Destinos</Button>
+        <Button component={Link} to="/about" color="inherit">Paquetes</Button>
+        <Button component={Link} to="/contact" color="inherit">Información</Button>
+        <Button component={Link} to="/login" variant="outlined" color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default Navbar
-
+export default Navbar;
