af4bd7c907e95d53702349d69cb669824551c818
Facundo1414 <facu.allende14gmail.com>
2024-04-04 13:59:18 -0300

    volviendo al commit anterior

--- src/main/java/com/viajes/Travel/platform/entity/viajes.java
+++ src/main/java/com/viajes/Travel/platform/entity/viajes.java
@@ -1,35 +1,4 @@
 package com.viajes.Travel.platform.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-
-import java.util.Date;
-
-@Entity
-@Data
-
 public class viajes {
-
-    @jakarta.persistence.Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
- private Integer Id;
-
- @NotEmpty
-    private String Name;
-
- @NotNull
-    private Double Price;
-
- @NotEmpty
-    private String Destiny;
-
- @NotNull
-    private Date Date;
-
- private String Description;
 }
--- src/main/java/com/viajes/Travel/platform/respository/ViajesRepository.java
+++ src/main/java/com/viajes/Travel/platform/respository/ViajesRepository.java
@@ -1,13 +1,2 @@
-package com.viajes.Travel.platform.respository;
-
-
-import com.viajes.Travel.platform.entity.viajes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ViajesRepository extends JpaRepository<viajes,Double> {
-
-    List<viajes> findByPrice(Double price);
-
+package com.viajes.Travel.platform.respository;public interface ViajesRepository {
 }
