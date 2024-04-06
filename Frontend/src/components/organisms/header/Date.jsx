b4b7bcdaad4511d438f3d08cc168313d0d09321d
Facundo1414 <facu.allende14gmail.com>
2024-04-06 17:53:06 -0300

    se creo el searchbar del header, se agregaron nuevas dependencias y estilos

---
+++ Frontend/src/components/organisms/header/Date.jsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import dayjs from 'dayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+export default function Date({vueltaChecked}) {
+    const today = dayjs();
+    const [ida, setIda] = React.useState(dayjs(today));
+    const [vuelta, setVuelta] = React.useState(dayjs(today));
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer components={['DatePicker', 'DatePicker']}>
+            <DatePicker
+                label="IDA"
+                value={ida}
+                onChange={(newValue) => setIda(newValue)}
+                />
+                <DatePicker
+                label="VUELTA"
+                value={vuelta}
+                onChange={(newValue) => setVuelta(newValue)}
+                disabled={vueltaChecked}
+                />
+                
+            </DemoContainer>
+        </LocalizationProvider>
+);
+}
--- Frontend/src/components/organisms/header/Header.jsx
+++ Frontend/src/components/organisms/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './header.css'
+import SearchBar from './SearchBar';
 
 const Header = () => {
     return (
@@ -10,6 +11,8 @@ const Header = () => {
                     <h1 className='header_text'>FLY WITH THE BEST <span className='span_yellow'>QUALITY</span> AT THE BEST <span className='span_yellow'>PRICE</span></h1>
                 </div>
             </div>
+            <SearchBar/>
+
         </div>
     );
 };
