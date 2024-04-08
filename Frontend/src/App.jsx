cf62dcc41324ddb740b45a96a729adccdb448c7e
Facundo1414 <facu.allende14gmail.com>
2024-04-08 11:37:56 -0300

    se creo los cards del precio y hora para la pagina search. Tambien se creo la pagina /search

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes,Route } from 'react-router-dom'
 import { Main } from './pages/main/Main'
 import Navbar from './components/organisms/navbar/Navbar'
 import Footer from './components/organisms/footer/Footer'
+import Search from './pages/main/Search'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path="/" Component={Main}></Route>
-          <Route></Route>  
+          <Route path="/search" Component={Search}></Route>  
         </Routes>
         <Footer/>
       </>
---
+++ Frontend/src/pages/main/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Search_topCards from '../../components/organisms/Search_topCards/Search_topCards'
+
+const Search = () => {
+  return (
+    <div>
+        <Search_topCards/>
+    </div>
+  )
+}
+
+export default Search
