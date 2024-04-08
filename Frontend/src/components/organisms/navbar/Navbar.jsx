d01361311924809a418ae2dc440efe6d5108281a
Bruno Sosa <sosabruno3384@gmail.com>
2024-04-07 22:23:11 -0300

    Navbar now is responsive. Minor styles changes added

--- Frontend/src/components/organisms/navbar/Navbar.jsx
+++ Frontend/src/components/organisms/navbar/Navbar.jsx
@@ -1,30 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import { Box, Button, Typography, Toolbar, AppBar, IconButton, Menu, MenuItem } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import './Navbar.css'
 
-//TODO: hacer que el navbar sea responsive
 
 const Navbar = () => {
+  const [anchorNav, setAnchorNav] = useState(null);
+
+  const handleOpenNav = (event) => {
+    setAnchorNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorNav(null);
+  };
+
   return (
     <AppBar position="absolute" className="transparent-navbar">
       <Toolbar className="toolBar">
-        <Typography  variant="h6" component="div" sx={{ flexGrow: 1, color: 'white' }} className="logoContainer">
+        <Typography  variant="h6" component="div" sx={{color: 'white' }} className="logoContainer">
           <Link to="/" className="brand">FlyAirlines</Link>
         </Typography>
-        <div className="buttonContainer">          
+
+        <Box component='div' className="buttonContainer" sx={{display: { xs: 'flex', md: 'none'}}}>   
+          <IconButton onClick={handleOpenNav} >
+            <MenuIcon sx={{color: 'white'}}/>
+          </IconButton>
+          <Menu
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            anchorEl={anchorNav}
+            open={Boolean(anchorNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: {xs: 'block', md: 'none'},
+            }}
+          >
+            <MenuItem onClick={handleCloseNavMenu} component={Link} to="/destinos">Destinos</MenuItem>
+
+            <MenuItem onClick={handleCloseNavMenu} component={Link} to="/paquetes">Paquetes</MenuItem>
+
+            <MenuItem onClick={handleCloseNavMenu} component={Link} to="/informacion">Información</MenuItem>
+
+            <MenuItem sx={{justifyContent: 'center'}}>
+              <Button component={Link} to="/login" className="login-btn" >Login</Button>
+            </MenuItem>
+          </Menu>
+        </Box>
+
+        <Box sx={{display: {xs: 'none', md: 'flex'}}}>
           <Button component={Link} to="/destinos" color="inherit" className="nav-link" >Destinos</Button>
           <Button component={Link} to="/paquetes" color="inherit" className="nav-link">Paquetes</Button>
           <Button component={Link} to="/informacion" color="inherit" className="nav-link">Información</Button>
-          <Button component={Link} to="/login" variant="outlined" color="inherit" className="login-btn" >Login</Button>
-        </div>
+          <Button component={Link} to="/login"  color="inherit" className="login-btn" >Login</Button>
+        </Box>
       </Toolbar>
     </AppBar>
   );
 };
-
 
 export default Navbar;
