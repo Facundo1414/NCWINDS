89c0911b937b6ff415a06d47f57383bd94ca9e3d
Gustavo Martinez <gutamvol262@gmail.com>
2024-04-08 11:19:07 -0600

    creacion query para buscar por origen y destino

--- c17-37-m-java/src/main/java/com/viajes/Travel/platform/respository/ViajesRepository.java
+++ c17-37-m-java/src/main/java/com/viajes/Travel/platform/respository/ViajesRepository.java
@@ -1,14 +1,20 @@
 package com.viajes.Travel.platform.respository;
 
 
-import com.viajes.Travel.platform.entity.viajes;
+import com.viajes.Travel.platform.entity.Viajes;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ViajesRepository extends JpaRepository<viajes,Integer> {
+public interface ViajesRepository extends JpaRepository<Viajes,Integer> {
 
-    List<viajes> findByPrice(Double price);
+    List<Viajes> findByPrice(Double price);
+    
+   List<Viajes> findByDestiny(String destiny);
+   
+   List<Viajes> findByOriginsAndDestiny(String origins,String destiny);
+   
     
 
 }
