7993255fb58c1c032246c944b12dcab4d170686f
Bruno Sosa <sosabruno3384@gmail.com>
2024-04-11 17:47:51 -0300

    flightCards now are rendered from json examples

--- Frontend/src/components/organisms/flightDetails/FlightDetails.jsx
+++ Frontend/src/components/organisms/flightDetails/FlightDetails.jsx
@@ -1,18 +1,48 @@
-import React from "react";
-import { Box, Typography, Divider, Slider } from '@mui/material';
+import React, { useState } from "react";
+import { Box, Typography, Divider, Slider, Checkbox } from '@mui/material';
 
 
 const FlightDetails =()=>{
+  const [price, setPrice] = useState([0, 4000000]);
+  const [duracionVuelo, setDuracionVuelo] = useState(48);
 
-  
+  const handleChangePrice =(event, newPrice)=>{
+    setPrice(newPrice);
+  }
+
+  const handleChangeDuracion =(event, newTime)=>{
+    setDuracionVuelo(newTime);
+  }
+
   return(
-    <Box component='section' border='1px solid black' sx={{backgroundColor: 'white', color: 'black'}} borderRadius='12px' boxShadow='3' margin='8px' padding='12px'>
-      <Typography>Filtrar por</Typography>
+    <Box component='section' sx={{backgroundColor: 'white', color: 'black'}} borderRadius='12px' boxShadow='3' margin='8px' padding='20px'>
+      <Box component='div'>
+        <Typography variant="h6" component='h6'>Filtrar por</Typography>
+      </Box>
+      
       <Divider />
-      <Box>
+      <Box component='div' marginY='8px'>
         <Typography>Precio</Typography>
-        <Typography>$ 0 - $ 1.200.000</Typography>
-        <Slider />
+        <Typography>${price[0]} - ${price[1]}</Typography>
+        <Slider sx={{color: '#0658BE'}} value={price} onChange={handleChangePrice} valueLabelDisplay="auto" aria-labelledby="range-slider" min={0} max={4000000}/>
+      </Box>
+
+      <Divider />
+      <Box component='div' alignItems='center' marginY='6px'>
+        <Typography>Duración del vuelo</Typography>
+        <Slider sx={{color: '#0658BE'}} value={duracionVuelo} onChange={handleChangeDuracion} valueLabelDisplay="auto"  aria-labelledby="range-slider" min={0} max={48}/>
+      </Box>
+
+      <Divider />
+      <Box component='div' display='flex' alignItems='center' marginY='6px'>
+        <Checkbox />
+        <Typography>Hora de salida</Typography>
+      </Box>     
+
+      <Divider />
+      <Box component='div' display='flex' alignItems='center' marginY='6px'>
+        <Checkbox />
+        <Typography>Cantidad de escalas</Typography>
       </Box>
 
     </Box>
--- Frontend/src/pages/search/Search.jsx
+++ Frontend/src/pages/search/Search.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { Box, Grid, Typography } from '@mui/material'; 
+import { Grid} from '@mui/material'; 
 import FlightCard from "../../components/molecules/flightCard/FlightCard";
 import Search_topCards from '../../components/organisms/Search_topCards/Search_topCards'
-import SearchBar from '../../components/organisms/header/SearchBar'
 import FlightDetails from '../../components/organisms/flightDetails/FlightDetails';
+import jsonFlights from '../../utils/flightCard/flightCard.json';
 
 export const Search =()=>{
-  const salida = "Buenos Aires, Argentina";
-  let salidaSplit = salida.split(', ');
 
-  const destino = "Madrid, España";
-  let destunoSplit = destino.split(', ');
+  const splitLocation =(location)=>{
+    return location.split(', ');
+  }
 
   return(
     <Grid container marginTop={"5rem"}>
@@ -22,32 +21,18 @@ export const Search =()=>{
       
       <Grid item xs={9} paddingX={'20px'}>
         <Search_topCards/>
-        <FlightCard 
-          horaSalida={'12:00'} 
-          horaLlegada={'04:20'} 
-          lugarSalida={salidaSplit} 
-          lugarLlegada={destunoSplit} 
-          duracionVuelo={'10:45'} 
-          precio={'1.250.000'}
-        />
 
-        <FlightCard 
-          horaSalida={'12:00'} 
-          horaLlegada={'04:20'} 
-          lugarSalida={salidaSplit} 
-          lugarLlegada={destunoSplit} 
-          duracionVuelo={'10:45'} 
-          precio={'1.250.000'}
-        />
+        {jsonFlights.map((value)=>{
+          const newFligt ={
+            ...value,
+            origen: splitLocation(value.origen),
+            destino: splitLocation(value.destino),
+          }
+          
+          return <FlightCard key={newFligt.id} props={newFligt}/>
 
-        <FlightCard 
-          horaSalida={'12:00'} 
-          horaLlegada={'04:20'} 
-          lugarSalida={salidaSplit} 
-          lugarLlegada={destunoSplit} 
-          duracionVuelo={'10:45'} 
-          precio={'1.250.000'}
-        />
+        })}
+
       </Grid>
     </Grid>
   );
