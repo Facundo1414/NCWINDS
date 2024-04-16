278256b64e037ca704d61d1359fc0d545660ecce
marcorios26 <riosmar026@gmail.com>
2024-04-16 09:15:20 -0400

    added page to select seats for the flight

--- Frontend/src/pages/seatSelection/SeatSelection.jsx
+++ Frontend/src/pages/seatSelection/SeatSelection.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Button, Grid } from '@mui/material'
 import "./SeatSelection.css"
-import SelectFligth from '../../components/molecules/selectFligth/SelectFligth'
+import SelectFlight from '../../components/molecules/selectFligth/SelectFlight'
+
 export default function SeatSelection() {
   return (
     <div style={{height: "50rem"}}>
     <Grid container height={"100%"} padding={"1rem"} justifyContent={"space-evenly"}>
-        {/* <Grid xs={12} md={12} display={'flex'} justifyContent={'center'} alignItems={'center'} color={"black"} height={"5%"}>
-                <h2>Personal information and Payment</h2>
-        </Grid> */}
-        
-        <SelectFligth/>
+       
+        <SelectFlight/>
 
     </Grid>
 </div>
