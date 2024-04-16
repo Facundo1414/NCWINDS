f2084283f896ce4d2efbceb0aa34a3de5314116f
Facundo1414 <facu.allende14gmail.com>
2024-04-16 16:32:01 -0300

    acomodando elementos

--- Frontend/src/components/organisms/header/Header.jsx
+++ Frontend/src/components/organisms/header/Header.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import './header.css'
-import SearchBar from '../../molecules/searchBar/SearchBar';
+import React from "react";
+import "./header.css";
+import SearchBar from "../../molecules/searchBar/SearchBar";
 
 const Header = () => {
-    return (
-        <div >
-            <div className="heroSection">
-                <div className="overlay"></div>
-                <div className='header_textContainer'>
-                    <h1 className='header_text'>FLY WITH THE BEST <span className='span_yellow'>QUALITY</span> AT THE BEST <span className='span_yellow'>PRICE</span></h1>
-                </div>
-            </div>
-            <div className='searchBar'>
-                <SearchBar translateStyle={'true'}/>
-            </div>
-
+  return (
+    <div>
+      <div className="heroSection">
+        <div className="overlay"></div>
+        <div className="header_textContainer">
+          <h1 className="header_text">
+            FLY WITH THE BEST <span className="span_yellow">QUALITY</span> AT
+            THE BEST <span className="span_yellow">PRICE</span>
+          </h1>
         </div>
-    );
+      </div>
+      <div className="searchBar">
+        <SearchBar translateStyle={"true"} />
+      </div>
+    </div>
+  );
 };
 
-export default Header
+export default Header;
