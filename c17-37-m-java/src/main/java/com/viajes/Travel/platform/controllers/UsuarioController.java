83c6336dd99aec1a7a782d7aa1aff46cf068d20f
Gustavo Martinez <gutamvol262@gmail.com>
2024-04-22 11:57:30 -0600

    creacion de el edpoint de crear usuario al igual incriptado el password

--- c17-37-m-java/src/main/java/com/viajes/Travel/platform/controllers/UsuarioController.java
+++ c17-37-m-java/src/main/java/com/viajes/Travel/platform/controllers/UsuarioController.java
@@ -2,10 +2,13 @@ package com.viajes.Travel.platform.controllers;
 
 import com.viajes.Travel.platform.Dto.JwtResponseDto;
 import com.viajes.Travel.platform.Dto.LoginDto;
+import com.viajes.Travel.platform.Dto.UsuariosRequestDto;
 import com.viajes.Travel.platform.Dto.UsuariosResponseDto;
 import com.viajes.Travel.platform.entity.Usuario;
 import com.viajes.Travel.platform.entity.Viajes;
 import com.viajes.Travel.platform.jwt.JwtService;
+import com.viajes.Travel.platform.services.EstadosService;
+import com.viajes.Travel.platform.services.PerfilService;
 import com.viajes.Travel.platform.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -28,6 +32,12 @@ public class UsuarioController {
     private BCryptPasswordEncoder passwordEncode;
     @Autowired
     private JwtService jwtService;
+    
+    @Autowired
+    private PerfilService perfilService;
+    
+    @Autowired
+    private EstadosService estadosService;
 
 
     @PostMapping("/auth/login")
@@ -90,14 +100,48 @@ public class UsuarioController {
     	List<Usuario>datos = this.usuarioServ.listar();
     	
     	datos.forEach((dato)->{
-    		lista.add(new UsuariosResponseDto(dato.getId()
-    				,dato.getNombre(),dato.getCorreo(),
+    		lista.add(new UsuariosResponseDto(
+    				dato.getId(),
+    				dato.getNombre(),dato.getCorreo(),
     				dato.getPerfilId().getNombre(),
-    				dato.getPerfilId().getId(), dato.getEstadosId().getId()
-    				, dato.getEstadosId().getNombre()));
+    				dato.getPerfilId().getId(), dato.getEstadosId().getId(),
+    				dato.getEstadosId().getNombre()));
     	});
     	
     	return ResponseEntity.status(HttpStatus.OK).body(lista);
+    }
+    
+    
+    @PostMapping("/auth/usuarios")
+    
+    public ResponseEntity<?>crearUsuario(@RequestBody UsuariosRequestDto dto){
+    	
+    	Usuario usu=this.usuarioServ.buscarPorCorreo(dto.getCorreo());
+    	
+    	
+    	if(usu==null) {
+    		
+    		this.usuarioServ.guardar(new Usuario(dto.getCorreo()
+    				,dto.getNombre()
+    				,this.passwordEncode.encode(dto.getPassword())
+    				,"",new Date(),this.perfilService.buscarPorId(2L)
+    				,this.estadosService.buscarPorId(1L)));
+    		
+    		return ResponseEntity.status(HttpStatus.CREATED).body(new HashMap<String,String>() {
+    			{
+    				put("creado","se creo el usuario correctamente");
+    			}
+    		});
+    		
+    	}else {
+    		
+    		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new HashMap<String,String>() {
+    			{
+    				put("error","ocurrio un error inesperado");
+    			}
+    		});
+    	}
+    	
     }
 }
 
