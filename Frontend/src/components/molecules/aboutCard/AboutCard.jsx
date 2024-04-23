adfe84b63a4af5c8344f29bab7ddd5980eb61267
Facundo1414 <facu.allende14gmail.com>
2024-04-23 19:31:19 -0300

    arreglado

--- Frontend/src/components/molecules/aboutCard/AboutCard.jsx
+++ Frontend/src/components/molecules/aboutCard/AboutCard.jsx
@@ -7,11 +7,11 @@ import './AboutCard.css';
 const AboutCard =({ aboutImg, aboutTittle, aboutDesc })=>{
 
   return(
-    <Box className='aboutCard-container'>
+    <Box className='aboutCard-container' padding={'1rem'}>
       <Box className='aboutImg-container'>
         <img src={aboutImg} alt="img-about" />
       </Box>
-      <Box className='aboutText-container'>
+      <Box className='aboutText-container' justifyContent={'space-between'}>
         <Typography variant="h5">{aboutTittle}</Typography>
         <Typography variant="p">{aboutDesc}</Typography>
       </Box>
