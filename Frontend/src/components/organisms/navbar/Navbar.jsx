4d2a983500ec1ec17eb0c8f63d725b50bf592a65
Facundo1414 <facu.allende14gmail.com>
2024-04-24 13:07:59 -0300

    gap para separar los links del navbar

--- Frontend/src/components/organisms/navbar/Navbar.jsx
+++ Frontend/src/components/organisms/navbar/Navbar.jsx
@@ -111,7 +111,7 @@ const Navbar = (currentPage) => {
           </Menu>
         </Box>
 
-        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+        <Box sx={{ display: { xs: "none", md: "flex" } }} gap={'2rem'}>
           <Button
             component={Link}
             to="/search"
